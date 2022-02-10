c4f71f6e3bad416e718b4fee518925cbe3dd471f
RevelsXIX <joshua.revels@gmail.com>
2022-02-09 23:06:36 -0500

    third commit

--- lib/screens/alpha_screen.dart
+++ lib/screens/alpha_screen.dart
@@ -1,96 +1,92 @@
 import 'package:flutter/material.dart';
 
-// class AlphaScreen extends StatelessWidget { 
-//   @override 
-//   Widget build(BuildContext context) {
-//     return Container(
-//       color: Colors.amber[600],
-//       child: paddedPlaceholer(context)
-//     );
-//   }
-
-//   Widget paddedPlaceholer(context) {
-//     return Padding(
-//       padding: EdgeInsets.all(padding(context)),
-//       // padding: EdgeInsets.all(120),
-//       child: Image.asset(
-//         'assets/images/brews.jpg',
-//         scale: 8.0, )
-//       );
-//   }
-
-//   double padding(BuildContext context) {
-//     if (MediaQuery.of(context).orientation == Orientation.landscape){
-//       return MediaQuery.of(context).size.width * 0.1;
-//     } else {
-//       return MediaQuery.of(context).size.width * 0.3;
-//     }
-
-//   }
-
-
-//   double scale(BuildContext context) {
-//     if (MediaQuery.of(context).orientation == Orientation.landscape){
-//       return MediaQuery.of(context).size.height * 3;
-//     } else {
-//       return MediaQuery.of(context).size.height * 3;
-//     }
-
-//   }
-
-// }  
-
-
-
-
-
-
-
 class AlphaScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.indigo[900],
-        body: Stack(
-          children: <Widget>[
-            Positioned(
-                width: MediaQuery.of(context).size.width,
-                top: MediaQuery.of(context).size.width * 0.10,//TRY TO CHANGE THIS **0.30** value to achieve your goal 
-                child: Container(
-                  margin: EdgeInsets.all(16.0),
-                  child:Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Image.asset('assets/images/brews.jpg', scale: 10),
-                        SizedBox(height: 20,),
-                        Text("Joshua Revels", style: TextStyle(
-                            fontSize: 30, 
-                            color: Colors.greenAccent[700],
-                            fontFamily: 'Open Sans',
-                            fontWeight: FontWeight.bold
-                            )
-                        ),
-                        Text("Drinker of Brews", style: TextStyle(
-                            fontSize: 18, 
-                            color: Colors.greenAccent[700],
-                            fontFamily: 'Open Sans',
-                            fontWeight: FontWeight.normal
-                            )
-                        ),
-                        Text("Haver of Good Times", style: TextStyle(
-                            fontSize: 18, 
-                            color: Colors.greenAccent[700],
-                            fontFamily: 'Open Sans',
-                            fontWeight: FontWeight.normal
-                            )
-                        ),
-                      ]
-                  ),
-                ))
-          ],
-        )
+        body: Column( 
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,    
+            children: [
+            Padding(
+              padding: EdgeInsets.all(9),
+              child: SizedBox(
+                child: Image.asset('assets/images/brews.jpg'),
+            width: 100, height: 100))
+          ]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center, 
+            children: [
+            SizedBox(child: Text(
+              "Joshua Revels", 
+              style: TextStyle(
+                      fontSize: 25, 
+                      color: Colors.greenAccent[700],
+                      fontFamily: 'Open Sans',
+                      fontWeight: FontWeight.bold
+                      )),
+             height: 26),
+          ]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center, 
+            children: [
+            SizedBox(child: Text(
+              "Drinker of Brews", 
+              style: TextStyle(
+                      fontSize: 18, 
+                      color: Colors.greenAccent[700],
+                      fontFamily: 'Open Sans',
+                      fontWeight: FontWeight.normal
+                      )),
+             height: 26),
+          ]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center, 
+            children: [
+            SizedBox(child: Text(
+              "412 666 2337", 
+              style: TextStyle(
+                      fontSize: 18, 
+                      color: Colors.greenAccent[700],
+                      fontFamily: 'Open Sans',
+                      fontWeight: FontWeight.normal
+                      )),
+             height: 26),
+          ]),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center, 
+            children: [
+              Expanded(child: 
+            SizedBox(
+              child: Text(
+              "412 666 2337",
+              textAlign: TextAlign.center, 
+              style: TextStyle(
+                      fontSize: 18, 
+                      color: Colors.greenAccent[700],
+                      fontFamily: 'Open Sans',
+                      fontWeight: FontWeight.normal
+                      )),
+            // width: 100,
+            height: 20)),
+            Expanded(child: 
+            SizedBox(child: Text(
+              "412 666 2337",
+              textAlign: TextAlign.center, 
+              style: TextStyle(
+                      fontSize: 18, 
+                      color: Colors.greenAccent[700],
+                      fontFamily: 'Open Sans',
+                      fontWeight: FontWeight.normal
+                      )),
+            // width: 100,
+            height: 20
+          )
+          )])
+      ])
     );
   }
-
-
 }
